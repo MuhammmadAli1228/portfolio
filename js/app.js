ff31481c93bbdfcc4f9b9b3cce38c149ae240563
MuhammadAli <example@gmail.com>
2021-09-26 04:37:39 +0300

    Muhammad Ali

---
+++ js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+// const page = document.querySelector('.pages')
+// const n1 = document.querySelector('.none');
+// const n2 = document.querySelector('.none-2');
+
+// page.addEventListener('mouseenter', () => {
+
+//   n1.classList.toggle('none');
+//   n2.classList.toggle('none-2');
+
+//   n1.style.position = 'relative';
+//   n1.style.top = '50px';
+//   n1.style.right = '100px'
+//   n1.style.fontSize = '20px'
+//   n2.style.position = 'relative';
+//   n2.style.top = '80px';
+//   n2.style.right = '210px';
+//   n2.style.fontSize = '20px'
+
+
+
+//   // n1.style.display = 'block';
+//   // n2.style.display = 'block';
+// })
